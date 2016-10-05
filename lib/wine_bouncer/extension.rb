6a511c4aedbbccac75ffa43147dab4626cdfe94b
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2016-10-05 14:17:25 +0300

    Inject inline defied scopes into route' description's :security hash. If auth_strategy is protected, inject Doorkeeper's default scopes into :security hash. Set :protected boolean on the route to make it fast available on requests' processing.

--- lib/wine_bouncer/extension.rb
+++ lib/wine_bouncer/extension.rb
@@ -3,14 +3,25 @@
 module WineBouncer
   module Extension
     def oauth2(*scopes)
-      scopes = Doorkeeper.configuration.default_scopes.all if scopes.all? { |x| x.nil? }
       description = route_setting(:description) || route_setting(:description, {})
-      # case WineBouncer.configuration.auth_strategy
-      # when :default
-      description[:auth] = { scopes: scopes }
-      # when :swagger
-      description[:authorizations] = { oauth2: scopes.map { |x| { scope: x } } }
-      # end
+
+      # Create :security hash if missing: Open API(Swagger) 2.0 compliance
+      description[:security] = Array.new unless description[:security]
+      security = description[:security]
+      security.unshift({ inline: scopes })
+      api_scopes_undefined = security.all? { |x|
+        x.each_value { |y|
+          y.empty? || y.all? { |z| z.blank?}
+        }
+      }
+
+      if WineBouncer.configuration.auth_strategy == :protected
+        # If no scopes defined on the API, use default Doorkeeper scopes, because of :protected auth_strategy
+        security.unshift({ doorkeeper: Doorkeeper.configuration.default_scopes.all }) if api_scopes_undefined
+        description[:protected] = true
+      else
+        description[:protected] = false if api_scopes_undefined
+      end
     end
 
     Grape::API.extend self
